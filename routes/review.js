8ddc213d5a1a66e9aab533509f024e1a9f06a417
chandra parmar <chandra.parmar2004@gmail.com>
2025-07-19 23:35:16 +0530

    controllers for reviews

--- routes/review.js
+++ routes/review.js
@@ -19,34 +19,15 @@ const validateReview=(req,res,next)=>{
         next()
     }
 }
+const reviewController = require('../controllers/reviews.js') 
 //review post route
-router.post("/",isLoggedIn,validateReview,wrapAsync(async(req,res)=>{
-    let listing= await Listing.findById(req.params.id)
-    let newReview =  new Review(req.body.review)
-    newReview.author = req.user._id
-    console.log(newReview)
-    listing.reviews.push(newReview)
-
-    await newReview.save()
-    await listing.save()
-    req.flash('success',"New review created")
-
-
-   res.redirect(`/listings/${listing._id}`)
-}))
+router.post("/",isLoggedIn,validateReview,wrapAsync(reviewController.createReview))
 
 
 //delete route
 
 router.delete('/:reviewId',isReviewAuthor,
-    wrapAsync(async(req,res)=>{
-        let{id,reviewId}= req.params
-
-        await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
-        await Review.findByIdAndDelete(reviewId)
-        req.flash('success',"review deleted")
-        res.redirect(`/listings/${id}`)
-    })
+    wrapAsync(reviewController.deleteReview)
 )
 
 module.exports=router
